ba62aa32f63047456459643f73aa23872c64b9b5
Aryan-202 <aryanvishwakarma275@gmail.com>
2026-09-18 23:38:50 +0530

    feat: enhance send_email function signature and add stubs for email operations

--- clara/connections/google/gmail.py
+++ clara/connections/google/gmail.py
@@ -4,7 +4,34 @@ This module will handle all the crud operations done by ai agent according to us
 All required consent will be taken via Clara mobile app, user needs to agree all terms
 and condition before using this feature of Clara.
 """
+import datetime
 
 
-def send_email() -> None:
+def send_email(
+    to: str, cc: str, bcc: str, subject: str, body: str, attachments: str
+) -> None:
+    pass
+
+
+def search_emails(query: str, folder: str, date_range: datetime, limit: int) -> None:
+    pass
+
+
+def get_email_content(message_id: str):
+    pass
+
+
+def summarize_thread(message_ids: str) -> str:
+    pass
+
+
+def update_draft(draft_id: str, new_content: str, new_recipients: str):
+    pass
+
+
+def move_email(message_id: str, target_folder: str):
+    pass
+
+
+def delete_email(message_id: str, parmanent: bool = False):
     pass
